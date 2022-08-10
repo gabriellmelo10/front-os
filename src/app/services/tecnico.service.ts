31e35b372e7b5ced89d8541d73ca8d65738b6aa5
Gabriel <gabriellemlo@gmail.com>
2022-08-10 16:01:40 -0300

    Implementação do componente delete para Tecnico (Parte 1)

---
+++ src/app/services/tecnico.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Tecnico } from '../models/tecnico';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TecnicoService {
+
+  baseUrl: String = environment.baseUrl;
+
+  constructor(
+    private http : HttpClient,
+    private snack : MatSnackBar) { }
+
+  findAll():Observable<Tecnico[]> {
+    const url = this.baseUrl + "/tecnicos";
+    return this.http.get<Tecnico[]>(url);
+  }
+
+  findById(id: any):Observable<Tecnico> {
+    const url = `${this.baseUrl}/tecnicos/${id}`;
+    return this.http.get<Tecnico>(url);
+  }
+
+  create(tecnico: Tecnico): Observable<Tecnico> {
+    const url = this.baseUrl + "/tecnicos";
+    return this.http.post<Tecnico>(url, tecnico);
+  }
+
+  update(tecnico: Tecnico):Observable<Tecnico> {
+    const url = `${this.baseUrl}/tecnicos/${tecnico.id}`;
+    return this.http.put<Tecnico>(url, tecnico);
+  }
+
+  message(msg: String): void {
+    this.snack.open(`${msg}`, 'ok', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 4000
+    })
+  }
+}
